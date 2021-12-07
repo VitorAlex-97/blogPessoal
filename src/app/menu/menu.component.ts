38f9258ed2bbdbf99d66995dc258fb3b27d2df8a
VitorAlex-97 <vitualex97@gmail.com>
2021-12-07 14:20:07 -0300

    @VitorAlex-97 [Task: Componente inicio criado, e outras lógicas]

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nome = environment.nome
+  foto = environment.foto
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+
+  ) { }
+
+  ngOnInit(){
+  }
+
+  sair() {
+    environment.nome = ''
+    environment.foto = ''
+    environment.tipo = ''
+    environment.token = ''
+    environment.usuario = ''
+    environment.id = 0
+
+    this.router.navigate(['/entrar'])
+
   }
 
 }
