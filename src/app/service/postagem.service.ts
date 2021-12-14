050aa15ee08292168fe664bdb6298539b7d61ab5
VitorAlex-97 <vitualex97@gmail.com>
2021-12-14 20:07:24 -0300

    @VitorAlex-97 [Task: método PUT de postagem implementado]

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -17,12 +17,24 @@ export class PostagemService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
+  deletePostagem(id: number){
+    this.http.delete(`http://localhost:8080/postagens/${id}`)
+  }
+
   getAllPostagem(): Observable<Postagem[]> {
     return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
   }
 
 }
